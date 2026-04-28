d0d910e6e2ecf63b835951ff846c8c59e7053452
dkdldtu <ymr061103@gmail.com>
2026-04-28 12:39:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const PUBLIC_DIR = path.join(__dirname, "public");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
