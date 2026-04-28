cce958b40ee5b21c6acee0920eaa087d2edc53c3
dkdldtu <ymr061103@gmail.com>
2026-04-28 13:15:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
 const express = require("express");
 const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
