e9c323beedfe6f67ac6715f91afbf5ef00318e53
dkdldtu <ymr061103@gmail.com>
2026-04-28 12:19:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
